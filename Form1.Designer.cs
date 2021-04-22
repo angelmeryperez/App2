dc017499cb45ea5bea07555eda1feec8c99cca41
Angel Beli <angelmeryperez@gmail.com>
2021-04-22 18:05:31 -0400

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace App2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.F1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.F2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(260, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(528, 426);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.F1,
+            this.F2});
+            this.dataGridView2.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(242, 426);
+            this.dataGridView2.TabIndex = 1;
+            // 
+            // F1
+            // 
+            this.F1.HeaderText = "F1";
+            this.F1.Name = "F1";
+            // 
+            // F2
+            // 
+            this.F2.HeaderText = "F2";
+            this.F2.Name = "F2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn F1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn F2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;// para conectarse a bases de datos
+
+namespace App2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        bool Bucle = true;
+
+        int D = 0, i = 0, Contador = 0;
+        int F = 0;
+        string BaseDatos;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            dataGridView2.Rows.Add(1400);
+
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                //DE ESTA MANERA FILTRAMOS TODOS LOS ARCHIVOS EXCEL EN EL NAVEGADOR DE ARCHIVOS
+                Filter = "Excel | *.xls;*.xlsx;",
+
+                //AQUÍ INDICAMOS QUE NOMBRE TENDRÁ EL NAVEGADOR DE ARCHIVOS COMO TITULO
+                Title = "Seleccionar Archivo"
+            };
+
+            //EN CASO DE SELECCIONAR EL ARCHIVO, ENTONCES PROCEDEMOS A ABRIR EL ARCHIVO CORRESPONDIENTE
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                dataGridView1.DataSource = ImportarDatos(openFileDialog.FileName);
+            }
+
+
+
+ 
+        DataView ImportarDatos(string nombrearchivo) //COMO PARAMETROS OBTENEMOS EL NOMBRE DEL ARCHIVO A IMPORTAR
+        {
+            
+            //UTILIZAMOS 12.0 DEPENDIENDO DE LA VERSION DEL EXCEL, EN CASO DE QUE LA VERSIÓN QUE TIENES ES INFERIOR AL DEL 2013, CAMBIAR A EXCEL 8.0 Y EN VEZ DE
+            //ACE.OLEDB.12.0 UTILIZAR LO SIGUIENTE (Jet.Oledb.4.0)
+            string conexion = string.Format("Provider = Microsoft.ACE.OLEDB.12.0; Data Source = {0}; Extended Properties = 'Excel 12.0;'", nombrearchivo);
+
+            OleDbConnection conector = new OleDbConnection(conexion);
+
+            conector.Open();
+
+            //DEPENDIENDO DEL NOMBRE QUE TIENE LA PESTAÑA EN TU ARCHIVO EXCEL COLOCAR DENTRO DE LOS []
+            OleDbCommand consulta = new OleDbCommand("select * from [Hoja1$]", conector);
+
+            OleDbDataAdapter adaptador = new OleDbDataAdapter
+            {
+                SelectCommand = consulta
+            };
+
+            DataSet ds = new DataSet();
+
+            adaptador.Fill(ds);
+
+            conector.Close();
+
+            return ds.Tables[0].DefaultView;
+
+
+        }
+            while (F < 100) // Este while imprime es la que se encargar de los numeros en la colunna 0
+            {
+
+                dataGridView2.Rows[F].Cells[0].Value = F;
+                dataGridView2.Rows[F].Cells[0].Value = Frecuencia();
+                F++;
+            }
+
+            int Frecuencia(int C = 0)
+            {
+
+                while (Bucle)
+                {
+                    dataGridView2.Rows[Contador].Cells[0].Value = Contador++;
+                    if (Contador == 101) { Bucle = false; }
+                  
+                }
+                return D;
+            }
+            Bucle = true;
+
+        }
+
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ////string conexion = "Provider = Microsoft.Jet.OLEDB.4.0;Data Source = C:/Users/Angel Mery Perez/Desktop/Loto/Programs con exel/Regristro Actualizado.xlsx ;Extended Properties = \"Exel 8.0;HDR = Yes\"";
+            //String conexion = "Provider=Microsoft.ACE.OLEDB.12.0; Data Source = C:/Users/Angel Mery Perez/Desktop/Loto/Programs con exel/Regristro Actualizado.xlsx; Extended Properties='Excel 12.0 Xml;HDR=YES;'";
+            //OleDbConnection conector = default(OleDbConnection);
+            //conector = new OleDbConnection(conexion);
+            //conector.Open();
+
+            //OleDbCommand consulta = default(OleDbCommand);
+            //consulta = new OleDbCommand("selet * from[Hoja1$]",conector);
+
+            //OleDbDataAdapter Adactador = new OleDbDataAdapter();
+            //Adactador.SelectCommand = consulta;
+
+            //DataSet DS = new DataSet();
+
+            //Adactador.Fill(DS);
+            //dataGridView1.DataSource = DS.Tables[0];
+            //conector.Close();
+
+
+        }
+    }
+}
